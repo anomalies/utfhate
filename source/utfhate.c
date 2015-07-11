11de9b084b31ce058c4cc5a24e8ae2e14fd7807c
Andrew Watts <andrew@anomalous.info>
2015-07-12 04:32:51 +0930

    Implemented the "count" command. It supports counting both characters (+1 for each UTF-8 sequence) and/or bytes (+1 for each UTF-8 sequence byte). Implemented the "delete" command. All UTF-8 sequences are stripped while maintaining all other characters. Added verbose/silent output options. Modified utfhate_consume_utf_char(char **) to correctly identify UTF-8 sequence lengths. If an invalid sequence is encountered it will silently continue consuming. Removed the spurious new-line putc(...) at the end of utfhate_command_search(const struct utfhate_options *).

--- source/utfhate.c
+++ source/utfhate.c
@@ -8,6 +8,7 @@
 
 #define UTFHATE_BUFFER_SIZE 4096
 #define UTFHATE_UNUSED(arg) (void)arg
+#define UTFHATE_DEFAULT_VERBOSE 0
 
 enum utfhate_command_index {
     UTFHATE_COMMAND_INDEX_SEARCH = 0,
@@ -16,8 +17,16 @@ enum utfhate_command_index {
     UTFHATE_COMMAND_INDEX_COUNT
 };
 
+enum utfhate_command_count_type {
+    UTFHATE_COMMAND_COUNT_TYPE_CHARACTERS = 1,
+    UTFHATE_COMMAND_COUNT_TYPE_BYTES = 2,
+    UTFHATE_COMMAND_COUNT_TYPE_BOTH = UTFHATE_COMMAND_COUNT_TYPE_BYTES | UTFHATE_COMMAND_COUNT_TYPE_CHARACTERS
+};
+
 struct utfhate_options {
     enum utfhate_command_index command;
+    
+    unsigned int verbose;
     
     FILE * source_stream;
     FILE * destination_stream;
@@ -26,6 +35,10 @@ struct utfhate_options {
         struct {
             const char * replacement;
         } as_replace;
+        
+        struct {
+            enum utfhate_command_count_type type;
+        } as_count;
     } arguments;
 };
 
@@ -79,6 +92,12 @@ static int utfhate_command_option_count(
     struct utfhate_options *options
 );
 
+static int utfhate_command_option_verbose(
+    int *arguments, 
+    char ***pargv, 
+    struct utfhate_options *options
+);
+
 static int utfhate_command_search(const struct utfhate_options *options);
 static int utfhate_command_delete(const struct utfhate_options *options);
 static int utfhate_command_replace(const struct utfhate_options *options);
@@ -91,12 +110,13 @@ static const struct utfhate_command_option utfhate_command_list[] = {
     { "--delete",  "-d", &utfhate_command_option_delete,  "Deletes all UTF-8 characters found in the input (without destroying the source.)" },
     { "--replace", "-r", &utfhate_command_option_replace, "Replaces all UTF-8 characters with a specified value."                            }, 
     { "--count",   "-c", &utfhate_command_option_count,   "Counts the number of UTF-8 characters in the input."                              },
+    { "--verbose", "-v", &utfhate_command_option_verbose, "Enables verbose output command output"                                            },
     
     { NULL, NULL, NULL, NULL }
 };
 
 static char line_buffer[UTFHATE_BUFFER_SIZE];
-static char marker_buffer[UTFHATE_BUFFER_SIZE];
+static char scratch_buffer[UTFHATE_BUFFER_SIZE];
 
 int main(int argc, char **argv) {
     struct utfhate_options options;
@@ -131,6 +151,7 @@ int main(int argc, char **argv) {
 
 static void utfhate_set_default_options(struct utfhate_options *options) {
     options->command = UTFHATE_COMMAND_INDEX_SEARCH;
+    options->verbose = UTFHATE_DEFAULT_VERBOSE;
     options->source_stream = stdin;
     options->destination_stream = stdout;
     
@@ -218,10 +239,40 @@ static int utfhate_command_option_replace(int *arguments, char ***pargv, struct 
 }
 
 static int utfhate_command_option_count(int *arguments, char ***pargv, struct utfhate_options *options) {
+    options->arguments.as_count.type = UTFHATE_COMMAND_COUNT_TYPE_CHARACTERS;
+    
+    if (*arguments > 0) {
+        char * argument = **pargv;
+        
+        if (strcmp(argument, "chars") == 0) {
+            options->arguments.as_count.type = UTFHATE_COMMAND_COUNT_TYPE_CHARACTERS;
+            
+            (*arguments)--;
+            (*pargv)++;
+        } else if (strcmp(argument, "bytes") == 0) {
+            options->arguments.as_count.type = UTFHATE_COMMAND_COUNT_TYPE_BYTES;
+            
+            (*arguments)--;
+            (*pargv)++;
+        } else if (strcmp(argument, "both") == 0) {
+            options->arguments.as_count.type = UTFHATE_COMMAND_COUNT_TYPE_BOTH;
+            
+            (*arguments)--;
+            (*pargv)++;
+        }
+    }
+    
+    options->command = UTFHATE_COMMAND_INDEX_COUNT;
+    
+    return 0;
+}
+
+static int utfhate_command_option_verbose(int *arguments, char ***pargv, struct utfhate_options *options)
+{
     UTFHATE_UNUSED(arguments);
     UTFHATE_UNUSED(pargv);
     
-    options->command = UTFHATE_COMMAND_INDEX_COUNT;
+    options->verbose = 1;
     
     return 0;
 }
@@ -230,7 +281,7 @@ static int utfhate_command_search(const struct utfhate_options *options) {
     unsigned int line = 1, utf_total = 0;
     
     while (fgets(line_buffer, sizeof(line_buffer), options->source_stream) != NULL) {
-        char *offset, *marker_offset = marker_buffer;
+        char *offset, *marker_offset = scratch_buffer;
         unsigned int utf_found = 0;
         
         for(offset = line_buffer; *offset != '\0'; ++offset, ++marker_offset) {
@@ -263,18 +314,36 @@ static int utfhate_command_search(const struct utfhate_options *options) {
             fprintf(options->destination_stream, "Line %u, %u occurence(s):\n", line, utf_found);
             fputs(line_buffer, options->destination_stream);
             putc('\n', options->destination_stream);
-            fputs(marker_buffer, options->destination_stream);
-            putc('\n', options->destination_stream);
+            fputs(scratch_buffer, options->destination_stream);
         }
     }
     
-    fprintf(options->destination_stream, "UTF-8 characters found: %u\n", utf_total);
+    if (options->verbose != 0) {
+        fprintf(options->destination_stream, "UTF-8 characters found: %u\n", utf_total);
+    }
     
     return 0;
 }
 
 static int utfhate_command_delete(const struct utfhate_options *options) {
-    UTFHATE_UNUSED(options);
+    while (fgets(line_buffer, sizeof(line_buffer), options->source_stream) != NULL) {
+        char *offset, *output_offset = scratch_buffer;
+        
+        for(offset = line_buffer; *offset != '\0'; ++offset, ++output_offset) {
+            if (*offset < 0) {
+                if (utfhate_consume_utf_char(&offset) != 0)
+                    break;
+                
+                continue;
+            }
+            
+            *output_offset = *offset;
+        }
+        
+        *output_offset = '\0';
+        fputs(scratch_buffer, options->destination_stream);
+    }
+    
     return 0;
 }
 
@@ -284,21 +353,67 @@ static int utfhate_command_replace(const struct utfhate_options *options) {
 }
 
 static int utfhate_command_count(const struct utfhate_options *options) {
-    UTFHATE_UNUSED(options);
+    unsigned int utf_chars = 0, utf_bytes = 0;
+    
+    while (fgets(line_buffer, sizeof(line_buffer), options->source_stream) != NULL) {
+        char *offset;
+        
+        for(offset = line_buffer; *offset != '\0'; ++offset) {
+            if (*offset < 0) {
+                char * old_offset = offset;
+                
+                if (utfhate_consume_utf_char(&offset) != 0)
+                    break;
+                
+                utf_bytes += (unsigned int)(offset - old_offset) + 1;
+                utf_chars++;
+            }
+        }
+    }
+    
+    if (options->verbose != 0) {
+        if (options->arguments.as_count.type & UTFHATE_COMMAND_COUNT_TYPE_BYTES)
+            fprintf(options->destination_stream, "UTF-8 Bytes: %u\n", utf_bytes);
+        
+        if (options->arguments.as_count.type & UTFHATE_COMMAND_COUNT_TYPE_CHARACTERS)
+            fprintf(options->destination_stream, "UTF-8 Characters: %u\n", utf_chars);
+        
+    } else {
+        if (options->arguments.as_count.type & UTFHATE_COMMAND_COUNT_TYPE_BYTES)
+            fprintf(options->destination_stream, "%u\n", utf_bytes);
+        
+        if (options->arguments.as_count.type & UTFHATE_COMMAND_COUNT_TYPE_CHARACTERS)
+            fprintf(options->destination_stream, "%u\n", utf_chars);
+        
+    }
+    
     return 0;
 }
 
 static int utfhate_consume_utf_char(char **pbuffer) {
-    char *buffer = *pbuffer;
+    unsigned char *buffer = (unsigned char *)*pbuffer;
+    unsigned int expected_length = 1;
+
+    if (*buffer >= 0xC0 && *buffer < 0xE0) {
+        expected_length = 2;
+    } else if (*buffer >= 0xE0 && *buffer < 0xF0) {
+        expected_length = 3;
+    } else if (*buffer >= 0xF0 && *buffer < 0xF8) {
+        expected_length = 4;
+    } else if (*buffer >= 0xF8 && *buffer < 0xFC) {
+        expected_length = 5;
+    } else if (*buffer >= 0xFC) {
+        expected_length = 6;
+    }
     
-    while (*buffer != '\0') {
-        if (*buffer > 0) {
-            *pbuffer = buffer - 1;
+    for (; *buffer != '\0'; ++buffer) {
+        if (*buffer < 0x80 || expected_length == 0) {
+            *pbuffer = (char *)buffer - 1;
             
             return 0;
         }
         
-        buffer++;
+        --expected_length;
     }
     
     return -1;
